b4450f83e64ea2c3f5cfe09597aa3ff6b410f13a
Shivam Jakkanwar <shivamjakkanwar4@gmail.com>
2025-10-14 23:43:32 +0530

    Adding Post ,put and delete endpoint for employee

--- PerformanceEvaluationSystem/Controllers/EmployeeController.cs
+++ PerformanceEvaluationSystem/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PerformanceEvaluationSystem.Data;
+using PerformanceEvaluationSystem.Dto;
 
 namespace PerformanceEvaluationSystem.Controllers
 {
@@ -30,6 +31,56 @@ namespace PerformanceEvaluationSystem.Controllers
                 return NotFound();
             }
             return Ok(employee);
+        }
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Post([FromBody] EmployeeDtoPayload employeeDto)
+        {
+            var payload= new Employee
+            {
+                Name = employeeDto.Name,
+                Position = employeeDto.Position,
+                TeamId = employeeDto.TeamId
+            };
+            dbcontext.Employees.Add(payload);
+            dbcontext.SaveChanges();
+            var response = new ResponseEmployee
+            {
+                EmployeeId = payload.EmployeeId,
+                Name = payload.Name,
+                Position = payload.Position,
+                TeamId = payload.TeamId
+            };
+            return CreatedAtAction(nameof(GetById), new { id = response.EmployeeId }, response);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult Put([FromRoute] Guid id, [FromBody] EmployeeDtoPayload employeeDto)
+        {
+            var existingEmployee = dbcontext.Employees.FirstOrDefault(e => e.EmployeeId == id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+            existingEmployee.Name = employeeDto.Name;
+            existingEmployee.Position = employeeDto.Position;
+            existingEmployee.TeamId = employeeDto.TeamId;
+            dbcontext.SaveChanges();
+           
+            return NoContent();
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var existingEmployee = dbcontext.Employees.FirstOrDefault(e => e.EmployeeId == id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+            dbcontext.Employees.Remove(existingEmployee);
+            dbcontext.SaveChanges();
+            return NoContent();
         }
     }
 }
